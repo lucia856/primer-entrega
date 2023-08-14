e327bc9067319b9ed7c583b453ac658c6dca653e
unknown <jesica.campetella@hotmail.com>
2023-08-14 12:27:17 -0300

    Handlebars was installed and configured

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const handlebars = require("express-handlebars")
+const __dirname = require("utils.js")
+
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 8080;
-
 app.use(express.json());
+
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname+"/views")
+app.set("view engine", "handlebars")
+app.use(express.static(__dirname+"/public"))
+
+const PORT = 8080;
 
 const productsRouter = require('./routes/products');
 app.use('/api/products', productsRouter);
@@ -16,4 +24,3 @@ app.use('/api/carts', cartsRouter);
 app.listen(PORT, () => {
   console.log(`Server listening on port${PORT}`);
 });
-
